f1688aeaa7fb5504153fdac4a7fa6b90b7da77b8
Dulguun Otgon <dulguun0225@gmail.com>
2022-12-27 22:51:13 +0800

    tag migration script

--- packages/plugin-forum-api/src/press-center-migration/migrateTags.js
+++ packages/plugin-forum-api/src/press-center-migration/migrateTags.js
@@ -1,16 +1,138 @@
 const example = {
-  _id: "Ye9oAxPGC5t3uN6xt",
+  _id: "5tedH3Ep3vTEjQijE",
   relatedIds: [],
   scopeBrandIds: [],
-  name: "t1",
+  name: "t1.1",
   type: "forum:post",
   colorCode: "#697689",
-  parentId: "",
-  order: "t1forum:post",
+  parentId: "Ye9oAxPGC5t3uN6xt",
+  order: "t1forum:post/t1.1forum:post",
   createdAt: {
     $date: {
-      $numberLong: "1670572868103",
+      $numberLong: "1672149431170",
     },
   },
   __v: 0,
 };
+
+const quintypeExample = {
+  "display-name": "Агентлаг",
+  name: "Агентлаг",
+  slug: "agent",
+  "external-id": 34281,
+  parent: {
+    name: "Мэдэх эрх",
+    slug: "medeh-erh",
+    "external-id": 34354,
+    parent_id: null,
+  },
+};
+
+const Random = require("meteor-random");
+const dotenv = require("dotenv");
+dotenv.config();
+const fs = require("fs");
+const path = require("path");
+
+const { connect, disconnect, Tags } = require("./db/index.js");
+const randomColorCode = require("./randomColorCode");
+
+const { MIG_DATA_DIR } = process.env;
+
+function readAllTagsFromFile() {
+  const filePath = path.join(MIG_DATA_DIR, "sections-00001.txt");
+  const fileContents = fs.readFileSync(filePath, "utf-8");
+  return fileContents
+    .split(/\r?\n/)
+    .filter(Boolean)
+    .map(JSON.parse);
+}
+
+async function getTagsByExternalId() {
+  const tags = await Tags()
+    .find({})
+    .toArray();
+
+  const tagsByExternalId = {};
+
+  tags.forEach((tag) => {
+    if (tag.externalId) {
+      tagsByExternalId[tag.externalId] = tag;
+    }
+  });
+
+  return tagsByExternalId;
+}
+
+async function migrateTags() {
+  await connect();
+
+  const now = new Date();
+  const qtSections = readAllTagsFromFile();
+
+  const allExternalIds = qtSections.map((s) => String(s["external-id"]));
+
+  const Col = Tags();
+
+  await Col.deleteMany({ externalId: { $in: allExternalIds } });
+
+  const qtParents = qtSections.filter((tag) => !tag.parent || !tag.parent["external-id"]);
+  const qtChildren = qtSections.filter((tag) => tag.parent && tag.parent["external-id"]);
+
+  const parents = qtParents.map((qt) => ({
+    _id: Random.id(),
+    relatedIds: [],
+    scopeBrandIds: [],
+    name: qt.name,
+    type: "forum:post",
+    colorCode: randomColorCode(),
+    parentId: "",
+    order: `${qt.name}forum:post`,
+    createdAt: now,
+    externalId: String(qt["external-id"]),
+    __v: 0,
+  }));
+
+  await Col.insertMany(parents);
+
+  const tagsByExternalId = await getTagsByExternalId();
+
+  qtChildren.forEach((qt) => {
+    const parentExternalId = qt.parent["external-id"];
+    const parent = tagsByExternalId[qt.parent["external-id"]];
+
+    if (!parent) {
+      console.log(parentExternalId);
+    }
+  });
+
+  const children = qtChildren
+    .map((qt) => {
+      const parentExternalId = qt.parent["external-id"];
+
+      const parent = tagsByExternalId[parentExternalId];
+
+      if (!parent) return null;
+
+      return {
+        _id: Random.id(),
+        relatedIds: [],
+        scopeBrandIds: [],
+        name: qt.name,
+        type: "forum:post",
+        colorCode: randomColorCode(),
+        parentId: parent._id,
+        order: `${parent.order}/${qt.name}forum:post`,
+        createdAt: now,
+        externalId: String(qt["external-id"]),
+        __v: 0,
+      };
+    })
+    .filter(Boolean);
+
+  await Col.insertMany(children);
+
+  await disconnect();
+}
+
+module.exports = migrateTags;
---
+++ packages/plugin-forum-api/src/press-center-migration/runMigrateTags.js
@@ -0,0 +1,3 @@
+const migrateTags = require('./migrateTags');
+
+migrateTags();
